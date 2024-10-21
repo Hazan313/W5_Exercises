6f203ab49e7123a5e3a09ed6f35121f85bf0e1f9
Hazan313 <szaidi@appdev.yearup.org>
2024-10-21 14:19:45 -0500

    1st Python class hello world!

---
+++ hello.py
@@ -0,0 +1,17 @@
+print('Hello World!')
+
+
+message = 'Hello World!'
+print(message)
+
+dollars = 3
+cents = 0.50
+
+print(type(dollars + cents))
+
+print(type(dollars))
+
+
+dollar = "3 dollars"
+cent = "fifty cents"
+print(dollar + "" "" + cent)

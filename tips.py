b3787e37b204a9f70b3a301881fc44ee80974359
Hazan313 <szaidi@appdev.yearup.org>
2024-10-22 14:41:29 -0500

    Added Exercise 2A incomplete

---
+++ tips.py
@@ -0,0 +1,18 @@
+import math
+
+# Define known values
+food_cost = 79.25
+tax = 6.54
+tip = 12.00
+
+# Calculate the unknown
+total_due = food_cost + tax + tip
+
+# Display the results
+# print("The total due is " + str(total_due)) # This converts the data from integer to string, so we can addd it to the string statement.
+print ("Food cost is " + str(food_cost) + " and tax is " + str(tax))
+print("Tip is " + str(tip))
+print("Total due is " + str(total_due)) 
+# Cannot accept copy and paste from textbook due to the different commas? # Ask Bess
+
+

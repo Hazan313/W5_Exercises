9f6a7497bc1042e56bf012c8d206293fac501e17
Hazan313 <szaidi@appdev.yearup.org>
2024-10-22 14:50:55 -0500

    Completed and added Exercise 2A Lab1

--- tips.py
+++ tips.py
@@ -11,8 +11,8 @@ total_due = food_cost + tax + tip
 # Display the results
 # print("The total due is " + str(total_due)) # This converts the data from integer to string, so we can addd it to the string statement.
 print ("Food cost is " + str(food_cost) + " and tax is " + str(tax))
-print("Tip is " + str(tip))
+# print("Tip is " + str(tip))
 print("Total due is " + str(total_due)) 
 # Cannot accept copy and paste from textbook due to the different commas? # Ask Bess
 
-
+print("Tip is " + format(tip, '.2f'))
